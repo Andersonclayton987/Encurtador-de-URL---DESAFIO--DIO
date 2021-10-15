17288ab0616e9ef272cf28af2b93d66589c6aea5
Andersonclayton987 <andersonclayton987@gmail.com>
2021-10-14 23:26:09 -0300

    Encurtador-URL

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://admin:<password>@cluster0.4j5s4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
